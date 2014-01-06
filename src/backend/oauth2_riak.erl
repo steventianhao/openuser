31172dcb8a82921a223463ed9c3e56e9eac1ea95
erlangxk <erlang@viossolution.com>
2014-01-06 19:04:31 +0800

    add comment

--- src/backend/oauth2_riak.erl
+++ src/backend/oauth2_riak.erl
@@ -1,10 +1,12 @@
 -module(oauth2_riak).
 
--export([get_redirection_uri/1]).
+-export([get_redirect_uri/1]).
 
 -define(BUCKET_CLIENT,<<"clients">>).
 
-get_redirection_uri(ClientId)->
+% only return ok if redirect_uri is non-empty string, else return {error,notfound}
+%% TODO
+get_redirect_uri(ClientId)->
 	case riakcp:exec(get,[?BUCKET_CLIENT,ClientId]) of
 		NotFound={error,notfound} -> NotFound;
 		{ok,Obj} -> {ok, binary_to_term(riakc_obj:get_value(Obj))}
